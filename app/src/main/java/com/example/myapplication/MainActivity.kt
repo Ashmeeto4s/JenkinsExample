6335d4905fdb189e91689f39c1f2e299b75ad61c
Ashmeeto4s <ashmeet.arora@original4sure.com>
2021-03-12 21:50:08 +0530

    second commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val data =System.getenv("BaseUrl")
+        Toast.makeText(this,data,Toast.LENGTH_LONG).show()
 
     }
 }

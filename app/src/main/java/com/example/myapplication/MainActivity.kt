ecc0053ff662465fd5d7f96e7439ce7ea2b87cf8
Ashmeeto4s <ashmeet.arora@original4sure.com>
2021-05-24 17:09:39 +0530

    added build type

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.type).setText(BuildConfig.BUILD_TYPE)
         Toast.makeText(this,BuildConfig.SERVER_URL,Toast.LENGTH_LONG).show()
 
     }
